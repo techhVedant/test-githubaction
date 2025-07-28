ff6529f6463d0392e041e6e48649565f43fa36f6
Vedant Chaudhry <techhforvedant@gmail.com>
2025-07-28 18:48:11 +0530

    Class and test method added

---
+++ tests/test_operation.py
@@ -0,0 +1,10 @@
+from src.math_operations import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(4,2)==6
+    assert add(4+5)==9
+
+def test_sub():
+    assert sub(10,2)==8
+    assert sub(7,3)==4
